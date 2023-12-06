b0efb9a47c6fe1f0248423f38aefabed30618848
XcarabuzX <xcarabuzx@gmail.com>
2023-12-06 20:45:57 -0300

    🚧 styles for button states

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
   theme: {
     colors:{
       'primary-tomato':'hsl(4, 100%, 67%)',
+      'secondary-tomato':'hsl(26,97%,63%,1)',
       'dark-slate-grey':'hsl(234, 29%, 20%)',
       'charcoal-grey':'hsl(235, 18%, 26%)',
       'grey':'hsl(231, 7%, 60%)',
@@ -21,5 +22,6 @@ export default {
       },
     },
   },
+
   plugins: [],
 }

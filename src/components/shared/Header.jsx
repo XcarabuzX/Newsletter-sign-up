adc99020accd67cad463c186612ff1feda6a30b3
XcarabuzX <xcarabuzx@gmail.com>
2023-12-06 12:43:03 -0300

    🚧 Styles for form

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,8 +1,8 @@
 import SignUpSVG from '../../assets/images/illustration-sign-up-mobile.svg?react';
 function Header() {
   return(
-      <header>
-        <SignUpSVG/>
+      <header className="w-full">
+        <SignUpSVG className="w-full h-auto"/>
       </header>
   );
 }

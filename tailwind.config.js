afff3040ebeeab2b3d678a4d07589fcadaf817c3
XcarabuzX <xcarabuzx@gmail.com>
2023-12-06 09:43:58 -0300

    📦 instalation and config tailwind, img

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'primary-tomato':'hsl(4, 100%, 67%)',
+      'dark-slate-grey':'hsl(234, 29%, 20%)',
+      'charcoal-grey':'hsl(235, 18%, 26%)',
+      'grey':'hsl(231, 7%, 60%)',
+      'white':'hsl(0, 0%, 100%)'
+    },
+    extend: {
+      fontFamily: {
+        'body': ['Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        'default': '16px',
+      },
+    },
+  },
+  plugins: [],
+}

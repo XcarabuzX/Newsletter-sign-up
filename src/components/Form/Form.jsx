e6dce8db350448311adacacaf4d9a83b7c6b9256
XcarabuzX <xcarabuzx@gmail.com>
2023-12-07 09:10:27 -0300

    📦  yup dependecy

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,46 @@
-import estilos from './Form.module.css';
+import { Formik } from "formik";
+import estilos from "./Form.module.css";
 function Form() {
-    return ( 
-        <form action="">
-            <label htmlFor="email" className={estilos.label}>Email address
-                <input type="email" name="email" id="email" placeholder="email@company.com" className={estilos.input}/>
-            </label>
-            <button className={estilos.boton}>Subscribe to monthly newsletter</button>
+  return (
+    <Formik
+      initialValues={{ email: "" }}
+        
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email" className={estilos.label}>
+            Email address
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="email@company.com"
+              className={estilos.input}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+          </label>
+          <button className={estilos.boton}>
+            Subscribe to monthly newsletter
+          </button>
         </form>
-     );
+      )}
+    </Formik>
+  );
 }
 
 export default Form;

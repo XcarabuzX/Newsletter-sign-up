b03b661e20b701841023e528bc5e95ed4cdc448f
JJozef <josemillaquen12@gmail.com>
2024-01-18 13:58:53 -0300

    ✨ Remove unused CSS class and update styling in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 
 import './App.css'
-import estilos from './App.module.css';
 import Form from './components/Form/Form'
 import Header from './components/shared/Header'
 import Main from './components/shared/Main'
@@ -8,7 +7,7 @@ import Main from './components/shared/Main'
 function App() {
 
   return (
-    <div className={estilos.tarjeta + " font-body"}>
+    <div className='sm:bg-white sm:flex sm:flex-row-reverse sm:rounded-3xl sm:w-3/5 font-body'>
       <Header/>
       <Main>
         <Form/>
